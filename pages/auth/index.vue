6bdd7aaf53358dacb1536d4aa80357a4f32e15ac
JoshFransix <joshfransix@gmail.com>
2023-09-12 13:55:08 +0100

    update favicon path

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -19,10 +19,10 @@
             class="right tw-select-none tw-pointer-events-none tw-bg-brand-primary tw-flex tw-justify-center tw-items-center tw-h-full">
             <!-- <landing-image class="tw-w-1/2 tw-h-1/2" /> -->
             <v-slide-x-reverse-transition leave-absolute hide-on-leave>
-                <img v-if="signUp" src="@/assets/svg/saly-11.svg" alt="" class="tw-w-2/3 tw-h-2/3">
+                <img v-if="signUp" src="@/assets/svg/Saly-11.svg" alt="" class="tw-w-2/3 tw-h-2/3">
             </v-slide-x-reverse-transition>
             <v-slide-x-reverse-transition leave-absolute hide-on-leave>
-                <img v-if="!signUp" src="@/assets/svg/saly-10.svg" alt="" class="tw-w-2/3 tw-h-2/3">
+                <img v-if="!signUp" src="@/assets/svg/Saly-10.svg" alt="" class="tw-w-2/3 tw-h-2/3">
             </v-slide-x-reverse-transition>
         </div>
         <div @click="$router.push('/')" class="tw-absolute tw-top-9 tw-left-9">
