d1d98b52047235092f9dc4d130ce458ffd977371
JoshFransix <joshfransix@gmail.com>
2023-09-12 13:18:04 +0100

    deleted nuxt-google-auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,12 +59,7 @@ export default defineNuxtConfig({
     "@nuxtjs/google-fonts",
     "@nuxtjs/supabase",
     "@pinia/nuxt",
-    "nuxt-vue3-google-signin",
   ],
-
-  googleSignIn: {
-    clientId: process.env.CLIENT_ID,
-  },
 
   pinia: {
     autoImports: [
